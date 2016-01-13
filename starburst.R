99f734ff41c9fc736129483017d08fe9fefbd9fb
watsyurname529 <watsyurname529@yahoo.com>
2016-01-13 13:59:31 -0500

    Fixed spelling error and increased y-axis title offset

--- starburst.R
+++ starburst.R
@@ -15,13 +15,13 @@ sb_colors_cb <- c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c")
 #First, make a stacked bar chart
 sb_stack_bar <- ggplot(data = starburst, aes(x = Ranking, y = Votes, fill = Flavor)) + geom_bar(stat="identity")
 
-sb_stack_bar <- sb_stack_bar + labs(title = "Orginial Starburst Flavor Popularity", y = "Num. of Votes", x = "Ranking (1 is best, 4 is worst)")
+sb_stack_bar <- sb_stack_bar + labs(title = "Orginal Starburst Flavor Popularity", y = "Num. of Votes", x = "Ranking (1 is best, 4 is worst)")
 
 sb_stack_bar <- sb_stack_bar + scale_fill_manual(values=sb_colors)
 
 sb_stack_bar <- sb_stack_bar + theme(plot.title = element_text(vjust=2))
 sb_stack_bar <- sb_stack_bar + theme(axis.title.x = element_text(vjust=-0.35))
-sb_stack_bar <- sb_stack_bar + theme(axis.title.y = element_text(vjust=0.35))
+sb_stack_bar <- sb_stack_bar + theme(axis.title.y = element_text(vjust=0.45))
 
 ggsave("sb_stack_bar.png", width = 5, height = 5)
 ggsave("sb_stack_bar.pdf", width = 5, height = 5)
@@ -34,13 +34,13 @@ ggsave("sb_stack_bar_cb.pdf", width = 5, height = 5)
 #Next, make a side by side bar chart
 sb_side_bar <- ggplot(data = starburst, aes(x = Ranking, y = Votes, fill = Flavor)) + geom_bar(stat="identity", position="dodge")
 
-sb_side_bar <- sb_side_bar + labs(title = "Orginial Starburst Flavor Popularity", y = "Num. of Votes", x = "Ranking (1 is best, 4 is worst)")
+sb_side_bar <- sb_side_bar + labs(title = "Orginal Starburst Flavor Popularity", y = "Num. of Votes", x = "Ranking (1 is best, 4 is worst)")
 
 sb_side_bar <- sb_side_bar + scale_fill_manual(values=sb_colors)
 
 sb_side_bar <- sb_side_bar + theme(plot.title = element_text(vjust=2))
 sb_side_bar <- sb_side_bar + theme(axis.title.x = element_text(vjust=-0.35))
-sb_side_bar <- sb_side_bar + theme(axis.title.y = element_text(vjust=0.35))
+sb_side_bar <- sb_side_bar + theme(axis.title.y = element_text(vjust=0.45))
 
 ggsave("sb_side_bar.png", width = 5, height = 5)
 ggsave("sb_side_bar.pdf", width = 5, height = 5)
