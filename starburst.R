ba2fbcf428aac46e54d68418a2b566b53ceda450
watsyurname529 <watsyurname529@yahoo.com>
2016-01-13 13:56:51 -0500

    Added colorblind friendly palette

--- starburst.R
+++ starburst.R
@@ -6,8 +6,10 @@ library(ggplot2)
 #Read in starburst csv file
 starburst <- read.csv("starburst.csv", header=TRUE)
 
-#Set up starburst colors
+#Set up starburst colors, both in colors corresponding to the original
+#flavors and a colorblind friendly palette
 sb_colors <- c("#ff6600", "#ff6699", "#cc0000", "#ffff00")
+sb_colors_cb <- c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c")
 
 #Plot the data
 #First, make a stacked bar chart
@@ -24,6 +26,11 @@ sb_stack_bar <- sb_stack_bar + theme(axis.title.y = element_text(vjust=0.35))
 ggsave("sb_stack_bar.png", width = 5, height = 5)
 ggsave("sb_stack_bar.pdf", width = 5, height = 5)
 
+sb_stack_bar_cb <- sb_stack_bar + scale_fill_manual(values=sb_colors_cb)
+
+ggsave("sb_stack_bar_cb.png", width = 5, height = 5)
+ggsave("sb_stack_bar_cb.pdf", width = 5, height = 5)
+
 #Next, make a side by side bar chart
 sb_side_bar <- ggplot(data = starburst, aes(x = Ranking, y = Votes, fill = Flavor)) + geom_bar(stat="identity", position="dodge")
 
@@ -37,3 +44,8 @@ sb_side_bar <- sb_side_bar + theme(axis.title.y = element_text(vjust=0.35))
 
 ggsave("sb_side_bar.png", width = 5, height = 5)
 ggsave("sb_side_bar.pdf", width = 5, height = 5)
+
+sb_side_bar_cb <- sb_side_bar + scale_fill_manual(values=sb_colors_cb)
+
+ggsave("sb_side_bar_cb.png", width = 5, height = 5)
+ggsave("sb_side_bar_cb.pdf", width = 5, height = 5)
